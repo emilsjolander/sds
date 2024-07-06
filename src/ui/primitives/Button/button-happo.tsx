1fa73089d43ca268037a741f30085681777099e3
Emil Sjölander <esjolander@figma.com>
2024-07-06 16:34:53 +0100

    test

--- src/ui/primitives/Button/button-happo.tsx
+++ src/ui/primitives/Button/button-happo.tsx
@@ -1,4 +1,4 @@
 import { Button } from './Button';
 
-export const primary = () => <Button variant="primary">Primary 1</Button>;
-export const subtle = () => <Button variant="subtle">Subtle 1</Button>;
+export const primary = () => <Button variant="primary">Primary 2</Button>;
+export const subtle = () => <Button variant="subtle">Subtle 2</Button>;
