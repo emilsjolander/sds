6248d64115fba1521855639a7cac2a031463b2d5
Emil Sjölander <esjolander@figma.com>
2024-07-08 09:11:03 +0100

    configure happo

--- .happo.js
+++ .happo.js
@@ -1,8 +1,11 @@
 const { RemoteBrowserTarget } = require('happo.io');
+const happoPluginStorybook = require('happo-plugin-storybook');
 
 module.exports = {
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
+
+  plugins: [happoPluginStorybook()],
 
   targets: {
     'chrome-desktop': new RemoteBrowserTarget('chrome', {
