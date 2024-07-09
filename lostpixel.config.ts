2376adbdbc555599cee6adb953b127e89561983e
Emil Sjölander <esjolander@figma.com>
2024-07-09 21:42:05 +0100

    setup lost pixel

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,7 @@ import { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
     storybookShots: {
-        storybookUrl: './storybook-static',
+        storybookUrl: './dist/storybook',
     },
     lostPixelProjectId: 'clyeun75h5rgf1trjvweb8k0m',
     apiKey: process.env.LOST_PIXEL_API_KEY,
